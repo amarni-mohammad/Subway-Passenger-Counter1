4ef419c439fb8fadf3967ce2f2a4d222df68df9a
amarni-mohammad <122675946+amarni-mohammad@users.noreply.github.com>
2023-01-15 16:48:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let increase = 0;
+
+ let savebt = document.getElementById("save-bt")
+
+ let increaseAL = document.getElementById("increase-AL")
+ 
+ function Press()
+{
+ 
+      increase = increase + 1
+     
+      increaseAL.textContent= increase
+   
+ }
+ 
+ function save()
+ {
+    document.getElementById("span").textContent= increase;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
